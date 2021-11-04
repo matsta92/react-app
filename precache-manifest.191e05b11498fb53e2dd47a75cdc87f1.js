57ed9424ff69948ffec754692f8ec89eb5cdaa13
Mateusz Stawiarz <matsta92@gmail.com>
2021-11-04 17:34:24 +0100

    Updates

---
+++ precache-manifest.191e05b11498fb53e2dd47a75cdc87f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce5bcf8c75a3f05b3117",
+    "url": "/react-app/static/css/main.5980839e.chunk.css"
+  },
+  {
+    "revision": "ce5bcf8c75a3f05b3117",
+    "url": "/react-app/static/js/main.27e65d49.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "4c59afacec54cb918c28",
+    "url": "/react-app/static/js/2.da5fdfa2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8b62a75bd6d0b1ac52d2f1a0cb9ce5b6",
+    "url": "/react-app/index.html"
+  }
+];
